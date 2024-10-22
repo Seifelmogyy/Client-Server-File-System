d150abb7dfc7dae8271a5a2320a39da6e01e72c9
Seifelmogyy <SE2100808@tkh.edu.eg>
2024-10-22 11:33:57 +0200

    Adding ssl Self Signed certificate

--- Server.py
+++ Server.py
@@ -1,16 +1,27 @@
 import http.server
 import socketserver
 import os
+import ssl
 
 
 PORT = 8000  # Feel free to use a different port
+
+
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
+
+
 os.chdir('/path/to/your/files')  # Replace with your directory path
+
+
 handler_object = MyHttpRequestHandler
+
+
 with socketserver.TCPServer(("", PORT), handler_object) as httpd:
+    httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="server.pem", certfile="server.pem", server_side=True)
+
     print(f"Serving at port {PORT}")
     print("Server is running. Press Ctrl+C to stop the server.")
     try:
